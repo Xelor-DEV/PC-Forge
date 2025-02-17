e1bb2f5f453ea3ac0e01c4dbba021695f24aaf70
Xelor-DEV <BladeUAOficial@gmail.com>
2025-02-17 00:42:18 -0500

    Save System scripts added

---
+++ Assets/ProjectAssets/Scripts/SceneLoader.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance { get; private set; }
+
+    [Header("Fade Settings")]
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 1f;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(LoadSceneWithFade(sceneName));
+    }
+
+    private IEnumerator LoadSceneWithFade(string sceneName)
+    {
+        yield return StartCoroutine(FadeOut());
+        SceneManager.LoadScene(sceneName);
+        yield return StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float timer = 0;
+        while (timer <= fadeDuration)
+        {
+            fadeImage.color = new Color(0, 0, 0, Mathf.Lerp(0, 1, timer / fadeDuration));
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float timer = 0;
+        while (timer <= fadeDuration)
+        {
+            fadeImage.color = new Color(0, 0, 0, Mathf.Lerp(1, 0, timer / fadeDuration));
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        SaveManager.Instance.SaveGame();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
